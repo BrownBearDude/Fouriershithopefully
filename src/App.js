e1da90da4c03598eb4a1cdec222a301d3b37607a
JOBLIN <tjobl10@eq.edu.au>
2021-04-30 17:11:50 +1000

    Piano and stuff to circles

--- src/App.js
+++ src/App.js
@@ -1,89 +1,101 @@
-import React from 'react';
-import './App.css';
-import p5 from 'p5';
+import React, {useState} from 'react';
+import './index.css';
+import './styles.css';
+import { HexColorPicker, HexColorInput } from "react-colorful";
+import { useForm } from 'react-hook-form';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+import Code from './P5Code';
 
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.myRef = React.createRef()
-  }
-  
-  
-
-
-  Sketch = (p) => {
-    let cnv
-    var graphics
-    let angle = 0
-    let members;
-
-    let prevx = 0
-    let prevy = 0
-
-    
-
-    p.setup = () => {
-      p.frameRate(1000)
-      cnv = p.createCanvas(p.width, p.height);
-      graphics = p.createGraphics(document.getElementById('parent').offsetWidth, 600);
-      graphics.background(200)
-      members = [
-        {length: 100, color: 20, speed: 10},
-        {length:  100, color: 10, speed: - 11}
-      ]
-      p.resizeCanvas(document.getElementById('parent').offsetWidth, 600)
+export default function App() {
+  const [color, setColor] = useState("#000000");
+  const { register, watch } = useForm({
+    revalidateMode: 'onchange',
+    defaultValues : {
+      arms : 2,
+      masterSpeed : 0,
+      perframe : 5
     }
-    
-    p.windowResized = () => {
-      p.resizeCanvas(document.getElementById('parent').offsetWidth, 600)
-    }
-    
-
-    p.draw = () => {
-      p.background(155)
-      p.strokeWeight(10)
-      p.image(graphics, 0, 0)
-      
-
-      for (let n = 0; n < 5; n++) {
-        let previous_pos = [p.width/2, p.height/2];
-        let endx
-        let endy
-  
-        for (let i = 0; i < this.props.data.arms; i ++) {
-          let bruh_angle = angle*this.props.data["speed"+i]
-          let v = p5.Vector.fromAngle(bruh_angle);
-          endx = previous_pos[0]+v.x*this.props.data["length"+i]
-          endy = previous_pos[1]+v.y*this.props.data["length"+i]
-          p.line(previous_pos[0], previous_pos[1], endx, endy);
-          previous_pos = [endx, endy];
-        }
-        
-        graphics.fill(155);
-        graphics.stroke(0);
-        if (this.props.data.draw) {
-          graphics.line(prevx, prevy, endx, endy)
-        }
-        angle += 0.005 * this.props.data.masterSpeed/2
-        prevx = endx 
-        prevy = endy
-      }
-    }
-  }
-  componentDidMount() {
-    this.myP5 = new p5(this.Sketch, this.myRef.current)
-    
-  }
+  });
   
 
-  render() { 
-    return (
-      <div id="parent" ref={this.myRef}></div>
-    )
+  const formValues = watch();
+  const final = [];
+
+  for (let i = 0; i < formValues.arms; i++) {
+    final.push(
+      <Card key={i}>
+            <b>Arm {i+1}</b>
+            <br/>
+          <Form.Group>
+              <Form.Label>{"Length: "+formValues['length'+i]}</Form.Label>
+              <Form.Control type="range" min="1" max="100"  {...register("length"+i, { required: true })} defaultValue="2"></Form.Control>
+          </Form.Group>
+          <Form.Group>
+              <Form.Label>{"speed: "+formValues['speed'+i]}</Form.Label>
+              <Form.Control type="range" min="-10" max="10" step="0.5"  {...register('speed'+i, { required: true })} defaultValue="0"></Form.Control>
+          </Form.Group>
+        </Card> 
+    );
   }
+
+  return(
+    <Row>
+      <Col>
+        <Code data={formValues} color={color}></Code>
+      </Col>
+      <Col style={{maxHeight: 550, overflow: "auto"}}>
+      <Container>
+        <Form >
+        <Form.Group>
+            <Button id="reset" name="reset"> Reset</Button>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="draw">Draw</Form.Label>
+            <Form.Check id="draw"  defaultValue={false} {...register('draw', { required: true })}></Form.Check>
+          </Form.Group>
+          <hr/>
+          <Form.Group>
+            <Form.Label>{"Master Speed: "+formValues.masterSpeed}</Form.Label>
+            <Form.Control type="range" min="-5" max="5" step="0.05"  defaultValue="0" {...register('masterSpeed', { required: true })}></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>{"Scale: "+formValues.scale}</Form.Label>
+            <Form.Control type="range" min="0.05" max="20" defaultValue="1" step="0.25" {...register('scale', { required: true })} ></Form.Control>
+            <Form.Text>keep value low</Form.Text>
+          </Form.Group>
+          <div>
+            <HexColorPicker color={color} onChange={setColor} />
+            <HexColorInput color={color} onChange={setColor} />
+          </div>
+          <Form.Group>
+            <Form.Label>{"Scale: "+formValues.scale}</Form.Label>
+            <Form.Control type="range" min="0.05" max="20" defaultValue="1" step="0.25" {...register('scale', { required: true })} ></Form.Control>
+            <Form.Text>keep value low</Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>thickness</Form.Label>
+            <Form.Control type="number" min="1" max="50" defaultValue="2" {...register('thiccness', { required: true })} ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>times per frame</Form.Label>
+            <Form.Control type="number" min="1" max="50" defaultValue="2" {...register('perframe', { required: true })} ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Number of arms</Form.Label>
+            <Form.Control type="number" min="1" max="50" defaultValue="2" {...register('arms', { required: true })} ></Form.Control>
+          </Form.Group>
+          <hr/>
+          {final}
+        </Form>
+      </Container>
+      <br/>
+      </Col>
+    </Row>
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,81 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Form from 'react-bootstrap/Form';
-import { useForm } from 'react-hook-form';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import TopBar from './topBar'
+import Help from "./help"
+import Home from "./home"
+import KeyBoardNath from './keyboardNath';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function Bruh() {
-  const { register, watch } = useForm({
-    revalidateMode: 'onchange',
-    defaultValues: {
-      draw: false,
-      masterSpeed: "0",
-      arms: "2"
-    }
-  });
-  const formValues = (watch())
-
-  const final = [];
-  for (let i = 0; i<formValues.arms; i++) {
-    final.push( 
-      <Card key={i}>
-        <Form.Group>
-          <Form.Label>Length</Form.Label>
-          <Form.Control type="range" min="0" max="100" name={"length"+i} ref={register({required: true})} defaultValue="2"></Form.Control>
-          <Form.Label>Speed</Form.Label>
-          <Form.Control type="range" min="-100" max="100" name={"speed"+i} ref={register({required: true})} defaultValue="0"></Form.Control>
-        </Form.Group>
-      </Card>
-    );
-  }
-
-  
-  return(
-    <Row>
-      <Col>
-        <App data={formValues}></App>
-      </Col>
-      <Col xs={6}>
-        <Form>
-        <Form.Group>
-            <Button name="reset"> Reset</Button>
-          </Form.Group>
-          <Form.Group>
-            <Form.Check name="draw" ref={register({required: true})} defaultValue={false}></Form.Check>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Master Speed</Form.Label>
-            <Form.Control type="range" min="-3" max="3" name="masterSpeed" ref={register({required: true})} defaultValue="0"></Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Number of arms</Form.Label>
-            <Form.Control type="number" min="1" max="50" name="arms" ref={register({required: true})} defaultValue="2"></Form.Control>
-          </Form.Group>
-          {final}
-
-        </Form>
-      </Col>
-    </Row>
-  )
-}
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Bruh />
+    <div>
+      <BrowserRouter>
+        <TopBar/>
+        <Switch>
+          <Route path="/app" component={App}/>
+          <Route path="/help" component={Help}/>
+          <Route path="/bruh" component={KeyBoardNath}/>
+          <Route path="" component={Home}/>
+          
+        </Switch>
+      </BrowserRouter>
+
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
